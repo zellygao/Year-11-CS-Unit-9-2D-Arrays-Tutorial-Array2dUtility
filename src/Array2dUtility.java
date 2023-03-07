c8a46a136a69e9eac853bc4bdfe1906d78cc03b1
Adam <adam.ewing.nz@gmail.com>
2023-03-07 16:36:55 +0800

    First commit.

--- src/Array2dUtility.java
+++ src/Array2dUtility.java
@@ -1,2 +1,35 @@
-package PACKAGE_NAME;public class Array2dUtility {
+public class Array2dUtility {
+
+    // 1. print
+    // Implement a method called print that prints out the values of the 2D array of integers row by row.
+
+    // 2. sum
+    // Implement a method called sum that returns the sum of a 2D array of integers
+
+    // 3. average
+    // Implement a method called average that returns the average value of a 2D array of integers.
+
+    // 4. minimum
+    // Implement a method called minimum that returns the minimum value of a 2D array of integers.
+
+    // 5. maximum
+    // Implement a method called maximum that returns the maximum value of a 2D array of integers.
+
+    // 6. evenCountStandard
+    // Implement a method called evenCountStandard that returns the number of even numbers in a 2D array of integers.
+    // Use a standard for loop to implement this method.
+
+    // 7. evenCountEnhanced
+    // Implement a method called evenCountEnhanced that returns the number of even numbers in a 2D array of integers.
+    // Use an enhanced for loop to implement this method.
+
+    // 8. allPositive
+    // Implement a method called allPositive that returns true if all the values in a 2D array of integers are positive.
+
+    // 9. rowSums
+    // Implement a method called rowSums that returns a one dimensional array that contains the sum of each row at each index.
+
+    // 10. colSums
+    // Implement a method called colSums that returns a one dimensional array that contains the sum of each col at each index.
+
 }
--- src/Array2dUtilityTest.java
+++ src/Array2dUtilityTest.java
@@ -1,2 +1,69 @@
-package PACKAGE_NAME;public class Array2dUtilityTest {
+import static org.junit.jupiter.api.Assertions.*;
+
+public class Array2dUtilityTest {
+
+    private int[][] array2d;
+    private int[][] array2d2;
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+
+        array2d =
+                new int[][]{
+                        {1, -2, 3, 4, 5},
+                        {6, 7, -8, 9, 10},
+                        {11, 12, 13, 14, 15},
+                        {16, 17, 18, 19, 20},
+                        {21, -22, 23, 24, 25}
+                };
+
+        array2d2 =
+                new int[][]{
+                        {11, -2, 3, 14, 5},
+                        {6, 71, 28, 9, 30},
+                        {11, 123, -13, 14, 15},
+                        {16, 17, 108, -29, 20},
+                        {21, 22, 203, 24, 45}
+                };
+
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+        array2d = null;
+        array2d2 = null;
+    }
+
+    @org.junit.jupiter.api.Test
+    void sum() {
+        assertTrue(Array2dUtility.sum(array2d) == 261);
+        assertTrue(Array2dUtility.sum(array2d2) == 772);
+    }
+
+    @org.junit.jupiter.api.Test
+    void minimum() {
+        assertTrue(Array2dUtility.minimum(array2d) == -22);
+        assertTrue(Array2dUtility.minimum(array2d2) == -29);
+    }
+
+    @org.junit.jupiter.api.Test
+    void maximum() {
+        assertTrue(Array2dUtility.maximum(array2d) == 25);
+        assertTrue(Array2dUtility.maximum(array2d2) == 203);
+    }
+
+    @org.junit.jupiter.api.Test
+    void evenCountStandard() {
+        assertTrue(Array2dUtility.evenCountStandard(array2d) == 12);
+        assertTrue(Array2dUtility.evenCountStandard(array2d2) == 11);
+    }
+
+    @org.junit.jupiter.api.Test
+    void evenCountEnhanced() {
+        assertTrue(Array2dUtility.evenCountEnhanced(array2d) == 12);
+        assertTrue(Array2dUtility.evenCountEnhanced(array2d2) == 11);
+    }
+
+
+
 }
